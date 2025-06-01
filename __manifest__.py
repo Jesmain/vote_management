b439d63597d6a0c1a62fc41a93e224d61226b9a3
Jesús Rodríguez <jesusm.rodrod@educa.jcyl.es>
2025-06-01 19:11:05 +0200

    Commit inicial

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Vote Management",
+
+    'summary': """
+        Manage voting process and vote validation""",
+
+    'description': """
+        This module facilitates the process of voting by keeping track of voting areas, participating parties and allowing voters to check the validity of their vote
+    """,
+    'author': "Jesmain",
+    'website': "https://github.com/Jesmain/vote_management",
+    'category': 'Extra Tools',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
